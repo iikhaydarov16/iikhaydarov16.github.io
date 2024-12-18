4ee7b68e977931a80b7fee8e4f2acbcdb809efbd
ilnaz <iikhaydarov16@gmail.com>
2024-12-18 20:30:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".swiper-topics", {
+    slidesPerView: 4,
+    spaceBetween: 16,
+    freeMode: true,
+    navigation: {
+        nextEl: ".sw-button-next",
+        prevEl: ".sw-button-prev",
+      },
+  });
